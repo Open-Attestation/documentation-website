ccd596040aaad6b8bb71b1e0c8229cfef1173edb
Sim Boon Long <simboonlong@gmail.com>
2020-12-11 14:10:12 +0800

    feat: update logos (#57)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -15,13 +15,13 @@ const siteConfig = {
           path: "./docs",
           // Sidebars file relative to website dir.
           sidebarPath: require.resolve("./sidebars.json"),
-          editUrl: "https://github.com/Open-Attestation/documentation-website/tree/master/website"
+          editUrl: "https://github.com/Open-Attestation/documentation-website/tree/master/website",
         },
         theme: {
-          customCss: [require.resolve("./src/css/custom.css")]
-        }
-      }
-    ]
+          customCss: [require.resolve("./src/css/custom.css")],
+        },
+      },
+    ],
   ],
 
   themeConfig: {
@@ -29,12 +29,17 @@ const siteConfig = {
       title: "OpenAttestation",
       logo: {
         alt: "OpenAttestation Logo",
-        src: "img/logo.gif",
-        srcDark: "img/logo.gif"
+        src: "img/logos/oa.svg",
+        srcDark: "img/logos/oa.svg",
       },
-      items: [{ to: "docs/getting-started", label: "Docs", position: "left" }]
+      items: [{ to: "docs/getting-started", label: "Docs", position: "left" }],
     },
     footer: {
+      logo: {
+        alt: "GovTech Logo",
+        src: "img/logos/govtech-blue.png",
+        href: "https://www.tech.gov.sg",
+      },
       copyright: `Copyright © ${new Date().getFullYear()} Government Technology Agency (Singapore)`,
       links: [
         {
@@ -42,50 +47,50 @@ const siteConfig = {
           items: [
             {
               label: "Getting Started",
-              to: "docs/getting-started"
-            }
-          ]
+              to: "docs/getting-started",
+            },
+          ],
         },
         {
           title: "Community",
           items: [
             {
               label: "Project Chat",
-              href: "https://spectrum.chat/openattestation"
-            }
-          ]
+              href: "https://spectrum.chat/openattestation",
+            },
+          ],
         },
         {
           title: "More",
           items: [
             {
               label: "Github",
-              href: "https://github.com/Open-Attestation"
+              href: "https://github.com/Open-Attestation",
             },
             {
               html: `
                 <a href="https://www.netlify.com" target="_blank" rel="noreferrer noopener" aria-label="Deploys by Netlify">
                   <img src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg" alt="Deploys by Netlify" />
                 </a>
-              `
-            }
-          ]
-        }
-      ]
+              `,
+            },
+          ],
+        },
+      ],
     },
 
     algolia: {
       apiKey: "1c7e0f08161cfc504494cff933eb8a37",
       indexName: "openattestation",
-      algoliaOptions: {} // Optional, if provided by Algolia
+      algoliaOptions: {}, // Optional, if provided by Algolia
     },
     prism: {
-      theme: require("prism-react-renderer/themes/nightOwl")
+      theme: require("prism-react-renderer/themes/nightOwl"),
     },
     image: "img/undraw_tweetstorm.svg",
     metadatas: [{ name: "og:image", content: "img/undraw_online.svg" }],
-    sidebarCollapsible: true
-  }
+    sidebarCollapsible: true,
+  },
 };
 
 module.exports = siteConfig;

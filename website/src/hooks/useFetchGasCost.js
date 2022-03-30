7bbb8b11ab55b08917b1abbc9e21101544c4160e
Seth <hullogh@gmail.com>
2022-03-31 03:03:06 +0800

    feat: add polygon gas station

--- website/src/hooks/useFetchGasCost.js
+++ website/src/hooks/useFetchGasCost.js
@@ -1,16 +1,37 @@
 import React, { useEffect, useState } from "react";
 import { useRefresh } from './useRefresh';
 
-const fetchGasCostData = async () => {
+const gasApi = {
+  ethereum: "https://www.ethgasstation.info/json/ethgasAPI.json",
+  polygon: "https://gasstation-mainnet.matic.network/v2",
+};
+
+const parseGasRes = (chain, res) => {
+  switch (chain) {
+    case "ethereum":
+      return Number(res.average) / 10;
+    case "polygon":
+      return res.standard.maxFee;
+    default:
+      return 0;
+  }
+};
+
+const priceApi = {
+  ethereum: "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD",
+  polygon: "https://min-api.cryptocompare.com/data/price?fsym=MATIC&tsyms=USD",
+};
+
+const fetchGasCostData = async (chain) => {
   try {
     const [ethReq, gweiReq] = await Promise.all([
-      fetch("https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD"),
-      fetch("https://www.ethgasstation.info/json/ethgasAPI.json"),
+      fetch(priceApi[chain]),
+      fetch(gasApi[chain]),
     ]);
     const [ethRes, gweiRes] = await Promise.all([ethReq.json(), gweiReq.json()]);
     return {
       price: ethRes.USD,
-      gwei: gweiRes.average,
+      gwei: parseGasRes(chain, gweiRes),
     };
   } catch (e) {
     console.error(`Error: ${e.message}`);
@@ -18,13 +39,13 @@ const fetchGasCostData = async () => {
   }
 };
 
-export const useFetchGasCost = (interval = 0) => {
+export const useFetchGasCost = (chain, interval = 0) => {
   const [price, setPrice] = useState(0);
   const [gwei, setGwei] = useState(0);
   const tick = useRefresh(interval);
   let isMounted = true;
   const fetchData = async () => {
-    const data = await fetchGasCostData();
+    const data = await fetchGasCostData(chain);
     if (!isMounted || !data) return;
     setPrice(data.price);
     setGwei(data.gwei);

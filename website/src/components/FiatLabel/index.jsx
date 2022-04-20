c1e55750eb70d4ca60c3013f2438dace24e8fa32
Seth <hullogh@gmail.com>
2022-04-20 21:13:22 +0800

    feat: blink on price label changes

--- website/src/components/FiatLabel/index.jsx
+++ website/src/components/FiatLabel/index.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export const FiatLabel = ({ children, ...options }) => {
+  const [opacity, setOpacity] = useState(0);
+
+  useEffect(() => {
+    setOpacity(0);
+    setTimeout(() => setOpacity(1), 100);
+  }, [children]);
+
   const nf = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
     ...options,
   });
   const res = nf.format(Number(children));
-  return <span>{res}</span>;
+  return <span key={children} style={{opacity, transition: 'opacity 1s ease-in'}}>{res}</span>;
 };

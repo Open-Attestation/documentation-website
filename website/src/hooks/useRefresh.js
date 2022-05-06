56146ba3012c2db979080cd33ab92a71a57937b6
superical <superical@users.noreply.github.com>
2022-05-06 16:54:47 +0800

    docs: cost comparisons for beta and polygon (#128)
    * feat: contract cost savings comparisons
    
    * docs: add savings comparisons for versions and polygon
    
    * docs: update action names
    
    * docs: eip1559 compliant
    
    * refactor: change action labels
    
    * docs: add links to doc
    
    * docs: fix phrasing
    
    * docs: update gas costs
    
    * feat: blink on price label changes
    
    * feat: refresh only on tab active
    
    * refactor: change page title from review feedback

--- website/src/hooks/useRefresh.js
+++ website/src/hooks/useRefresh.js
@@ -1,11 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 export const useRefresh = (period) => {
   const [tick, setTick] = useState(0);
+  const isTabActive = useRef(true);
   if (period < 1) return undefined;
+
+  useEffect(() => {
+    const activeHandler = () => {
+      isTabActive.current = !document.hidden;
+    };
+    window.addEventListener("visibilitychange", activeHandler);
+    return () => window.removeEventListener("visibilitychange", activeHandler);
+  }, []);
+
   useEffect(() => {
     const interval = setInterval(() => {
-      setTick((tick) => tick + 1);
+      if (isTabActive.current) setTick((tick) => tick + 1);
     }, period);
     return () => clearInterval(interval);
   }, []);

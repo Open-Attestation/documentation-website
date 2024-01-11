7339100dcdd2067ba0983a3da2a9f338b98a62bf
Chloe Cai <93294344+rollerchloe@users.noreply.github.com>
2024-01-11 16:32:08 +0800

    docs: edit config.js and sidebars.json

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -31,7 +31,7 @@ const siteConfig = {
       {
         name: "OpenAttestation",
         sourceBaseUrl: "https://raw.githubusercontent.com/Open-Attestation/open-attestation/master/",
-        outDir: "docs/developer-section/libraries/remote-files/",
+        outDir: "docs/lib-section/remote-files/",
         documents: ["README.md"],
         modifyContent(fileName, content) {
           return {
@@ -53,7 +53,7 @@ ${content}`,
       {
         name: "OpenAttestation (CLI)",
         sourceBaseUrl: "https://raw.githubusercontent.com/Open-Attestation/open-attestation-cli/master/",
-        outDir: "docs/developer-section/libraries/remote-files/",
+        outDir: "docs/lib-section/remote-files/",
         documents: ["README.md"],
         modifyContent(fileName, content) {
           return {
@@ -75,7 +75,7 @@ ${content}`,
       {
         name: "OpenAttestation (Encryption)",
         sourceBaseUrl: "https://raw.githubusercontent.com/Open-Attestation/oa-encryption/master/",
-        outDir: "docs/developer-section/libraries/remote-files/",
+        outDir: "docs/lib-section/remote-files/",
         documents: ["README.md"],
         modifyContent(fileName, content) {
           return {
@@ -97,7 +97,7 @@ ${content}`,
       {
         name: "OpenAttestation (Verify)",
         sourceBaseUrl: "https://raw.githubusercontent.com/Open-Attestation/oa-verify/master/",
-        outDir: "docs/developer-section/libraries/remote-files/",
+        outDir: "docs/lib-section/remote-files/",
         documents: ["README.md"],
         modifyContent(fileName, content) {
           return {
@@ -120,7 +120,7 @@ ${content}`,
         name: "React components for decentralized renderer",
         sourceBaseUrl:
           "https://raw.githubusercontent.com/Open-Attestation/decentralized-renderer-react-components/master/",
-        outDir: "docs/developer-section/libraries/remote-files/decentralized-renderer/",
+        outDir: "docs/lib-section/remote-files/",
         documents: ["README.md"],
         modifyContent(fileName, content) {
           return {
@@ -143,7 +143,7 @@ ${content}`,
         name: "React template for decentralized renderer",
         sourceBaseUrl:
           "https://raw.githubusercontent.com/Open-Attestation/decentralized-renderer-react-template/master/",
-        outDir: "docs/developer-section/libraries/remote-files/decentralized-renderer/",
+        outDir: "docs/lib-section/remote-files/",
         documents: ["README.md"],
         modifyContent(fileName, content) {
           return {
@@ -165,7 +165,7 @@ ${content}`,
       {
         name: "Document store",
         sourceBaseUrl: "https://raw.githubusercontent.com/Open-Attestation/document-store/master/",
-        outDir: "docs/developer-section/libraries/remote-files/",
+        outDir: "docs/lib-section/remote-files/",
         documents: ["README.md"],
         modifyContent(fileName, content) {
           return {
@@ -187,7 +187,7 @@ ${content}`,
       {
         name: "Token registry",
         sourceBaseUrl: "https://raw.githubusercontent.com/Open-Attestation/token-registry/master/",
-        outDir: "docs/developer-section/libraries/remote-files/",
+        outDir: "docs/lib-section/remote-files/",
         documents: ["README.md"],
         modifyContent(fileName, content) {
           return {
@@ -215,14 +215,18 @@ ${content}`,
         srcDark: "img/logos/oa.svg",
       },
       items: [
-
-        { to: "docs/overview-section/overview", label: "Overview", position: "left" },
-
+        
         {
           type: 'dropdown',
           label: 'Issue/Revoke',
           position: 'left',
           items: [
+            {
+              label: 'Overview',
+              to: '/docs/overview-section/overview',
+              activeBasePath: '/docs/overview-section',
+            },
+
             {
               label: 'Prerequisites',
               to: '/docs/test1-section/prerequisites',
@@ -258,6 +262,48 @@ ${content}`,
         { to: "docs/verify-section/verify", label: "Verify", position: "left" },
 
         { to: "docs/distribute-section/distribute", label: "Distribute", position: "left" },
+
+        {
+          type: 'dropdown',
+          label: 'Resources',
+          position: 'left',
+          items: [
+            {
+              label: 'Libraries',
+              to: '/docs/lib-section/overview-lib',
+              activeBasePath: '/docs/lib-section',
+            },
+
+            {
+              label: 'Guides',
+              to: '/docs/guides-section/create-issuer',
+              activeBasePath: '/docs/guides-section',
+            },
+
+            {
+              label: 'Frequently asked questions',
+              to: '/docs/faq-section/faq01',
+              activeBasePath: '/docs/faq-section',
+            },
+            {
+              label: 'Glossary',
+              to: '/docs/glossary-section/glossary',
+              activeBasePath: '/docs/glossary-section/glossary',
+            },
+
+            {
+              label: 'Changelog',
+              to: '/docs/changelog-section/overview-changelog',
+              activeBasePath: '/docs/changelog-section',
+            },
+
+            {
+              label: 'Blockchain costs',
+              to: '/docs/costs-section/overview-costs',
+              activeBasePath: '/docs/costs-section',
+            },
+          ],
+        },
 
         // Select "Edit" > "Toggle Line Comment" to show or hide these sections below.
         // ** Start of comment **

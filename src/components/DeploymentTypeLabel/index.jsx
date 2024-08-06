5ed0aec8a0aaed85e7fda21f26b29b0d982611e9
Seth <hullogh@gmail.com>
2024-08-07 07:03:25 +0800

    fix: deployment type label key

--- src/components/DeploymentTypeLabel/index.jsx
+++ src/components/DeploymentTypeLabel/index.jsx
@@ -5,9 +5,9 @@ export const DeploymentTypeLabel = ({deploymentType, remarks}) => {
   return (
   <div>
     <div className="deploymentTypeContainer">
-      {deploymentType.map((type) => {
-          if (type === "DNS") return <div className="deploymentTypeDNS">DNS</div>
-          if (type === "DID") return <div className="deploymentTypeDID">DID</div>
+      {deploymentType.map((type, idx) => {
+          if (type === "DNS") return <div className="deploymentTypeDNS" key={idx}>DNS</div>
+          if (type === "DID") return <div className="deploymentTypeDID" key={idx}>DID</div>
     })}
     </div>
     <div className="deploymentTypeRemark">{remarks}</div>
